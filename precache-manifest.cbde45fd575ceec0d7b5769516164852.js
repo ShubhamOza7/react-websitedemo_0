256c2fc79cf5b1e55f696a121336f6d9f6f86bc3
shubham oza <shubhamoza@shubhams-MacBook-Air.local>
2021-01-02 13:35:10 +0530

    Updates

---
+++ precache-manifest.cbde45fd575ceec0d7b5769516164852.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e0caa409d199de95b2a6744b224bfb",
+    "url": "/react-websitedemo_0/index.html"
+  },
+  {
+    "revision": "593a9193339c41c27aa1",
+    "url": "/react-websitedemo_0/static/css/main.0e012e29.chunk.css"
+  },
+  {
+    "revision": "c5a9e3738ecc458b4915",
+    "url": "/react-websitedemo_0/static/js/2.95033c2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-websitedemo_0/static/js/2.95033c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593a9193339c41c27aa1",
+    "url": "/react-websitedemo_0/static/js/main.14955efa.chunk.js"
+  },
+  {
+    "revision": "b6ffabc56ddb5a00d656",
+    "url": "/react-websitedemo_0/static/js/runtime-main.3ae57ea8.js"
+  }
+]);
